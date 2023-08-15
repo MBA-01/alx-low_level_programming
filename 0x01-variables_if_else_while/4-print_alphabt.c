9e4ee8807ef3ad865f43012ca0d30293429c4a65
MBA-01 <bachriouijr@gmail.com>
2023-08-14 18:39:55 -0700

    no q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,9 +4,9 @@ int main(void)
 {
 	char c;
 
-	for (c='a'; c<= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		if(c != 'q' && c != 'e')
+		if (c != 'q' && c != 'e')
 			putchar(c);
 	}
 	putchar('\n');
