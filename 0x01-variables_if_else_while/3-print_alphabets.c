563e1199ae6397a2160e8dde8f36410a50277ae1
MBA-01 <bachriouijr@gmail.com>
2023-08-14 18:18:00 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,16 +3,14 @@
 int main(void)
 {
 
-        /* Get the character to be written */
-        char ch;
+	char ch;
 
-        /* Write the Character to stdout */
-        for (ch = 'a'; ch <= 'z'; ch++)
-                putchar(ch);
 
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
                 putchar(ch);
 
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
